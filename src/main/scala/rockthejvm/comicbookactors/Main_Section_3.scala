1ab9588eaac4edf4c68140f090412cb60613fd61
mveeprojects <markvallis1987@Gmail.com>
2024-07-29 18:38:45 +0100

    Adding more examples.

--- src/main/scala/rockthejvm/comicbookactors/Main_Section_3.scala
+++ src/main/scala/rockthejvm/comicbookactors/Main_Section_3.scala
@@ -5,8 +5,6 @@ import fs2.{Pipe, Pure, Stream}
 import rockthejvm.comicbookactors.Main_Section_2.{avengersActors, jlActors}
 import rockthejvm.comicbookactors.Model.Actor
 
-import scala.::
-
 // https://blog.rockthejvm.com/fs2/
 
 object Main_Section_3 extends IOApp {
@@ -39,20 +37,17 @@ object Main_Section_3 extends IOApp {
   // So, a Pipe[F[_], I, O] represents nothing more than a function between two streams,
   // the first emitting elements of type I, and the second emitting elements of type O.
   // The through method applies a pipe to a stream.
-  val fromActorToStringPipe: Pipe[IO, Actor, String] = in =>
-    in.map(actor => s"${actor.firstName} ${actor.lastName}")
-  def toConsole[String]: Pipe[IO, String, Unit] = in =>
-    in.evalMap(IO.println)
+  val fromActorToStringPipe: Pipe[IO, Actor, String] = in => in.map(actor => s"${actor.firstName} ${actor.lastName}")
+  def toConsole[String]: Pipe[IO, String, Unit]      = in => in.evalMap(IO.println)
   val stringNamesOfJLActors: Stream[IO, Unit] =
     jlActors
       .through(fromActorToStringPipe)
       .through(toConsole)
 
-  override def run(args: List[String]): IO[ExitCode] = {
+  override def run(args: List[String]): IO[ExitCode] =
 //    printedJLActors.compile.drain.as(ExitCode.Success)
 //    evalMappedJLActors.compile.drain.as(ExitCode.Success)
 //    evalTappedJLActors.compile.drain.as(ExitCode.Success)
     stringNamesOfJLActors.compile.drain.as(ExitCode.Success)
-  }
 
 }
