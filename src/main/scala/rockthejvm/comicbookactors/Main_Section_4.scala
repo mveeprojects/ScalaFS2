67dfab0a87d1c33723b81527026c5cfcb446d6db
mveeprojects <markvallis1987@Gmail.com>
2024-07-29 18:39:57 +0100

    Adding more examples.

---
+++ src/main/scala/rockthejvm/comicbookactors/Main_Section_4.scala
@@ -0,0 +1,12 @@
+package rockthejvm.comicbookactors
+
+import cats.effect.{ExitCode, IO, IOApp}
+import fs2.{Pipe, Pure, Stream}
+import rockthejvm.comicbookactors.Main_Section_2.{avengersActors, jlActors}
+import rockthejvm.comicbookactors.Model.Actor
+
+// https://blog.rockthejvm.com/fs2/
+
+object Main_Section_4 extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] = ???
+}
