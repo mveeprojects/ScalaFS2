afec5309057081b2ba19a2119f847c2168544d5f
mveeprojects <markvallis1987@Gmail.com>
2024-07-30 10:18:06 +0100

    Adding more examples.

--- src/main/scala/rockthejvm/comicbookactors/Main_Section_4.scala
+++ src/main/scala/rockthejvm/comicbookactors/Main_Section_4.scala
@@ -1,12 +1,46 @@
 package rockthejvm.comicbookactors
 
 import cats.effect.{ExitCode, IO, IOApp}
-import fs2.{Pipe, Pure, Stream}
-import rockthejvm.comicbookactors.Main_Section_2.{avengersActors, jlActors}
+import fs2.Stream
+import rockthejvm.comicbookactors.Main_Section_2.jlActors
 import rockthejvm.comicbookactors.Model.Actor
+
+import scala.util.Random
 
 // https://blog.rockthejvm.com/fs2/
 
 object Main_Section_4 extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] = ???
+
+  // No error handling
+  val savedJLActors: Stream[IO, Int] = jlActors.evalMap(ActorRepository.save)
+
+  // Basic error handling
+  val savedJLActorsWithErrorHandling: Stream[IO, AnyVal] =
+    savedJLActors.handleErrorWith(error => Stream.eval(IO.println(s"Error: ${error.getMessage}")))
+
+  // Error handling to Either
+  val attemptedSavedJlActors: Stream[IO, Either[Throwable, Int]] = savedJLActors.attempt
+
+  override def run(args: List[String]): IO[ExitCode] =
+//    savedJLActors.compile.drain.as(ExitCode.Success)
+
+//    savedJLActorsWithErrorHandling.compile.drain.as(ExitCode.Success)
+
+    attemptedSavedJlActors.evalMap {
+      case Left(error) => IO.println(s"Error: ${error.getMessage}")
+      case Right(id)   => IO.println(s"Saved actor with id: $id")
+    }.compile.drain.as(ExitCode.Success)
+
+  // Could you combine the use of evalTap and handling in some way to prevent the stream ending,
+  // but logging the problematic cases out via the tap?
+}
+
+object ActorRepository {
+  def save(actor: Actor): IO[Int] = IO {
+    println(s"Saving actor: $actor")
+    if (Random.nextInt() % 2 == 0) {
+      throw new RuntimeException("Something went wrong communicating with the persistence layer.")
+    }
+    actor.id
+  }
 }
---
+++ src/main/scala/rockthejvm/comicbookactors/Main_Section_5.scala
@@ -0,0 +1,26 @@
+package rockthejvm.comicbookactors
+
+import cats.effect.{ExitCode, IO, IOApp}
+import fs2.Stream
+import rockthejvm.comicbookactors.Main_Section_4.savedJLActors
+
+// https://blog.rockthejvm.com/fs2/
+
+object Main_Section_5 extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] = {
+    val acquire = IO {
+      val conn = DatabaseConnection("jlaConnection")
+      println(s"Acquiring connection to database: $conn")
+      conn
+    }
+
+    val release = (conn: DatabaseConnection) => IO.println(s"Releasing connection to database: $conn")
+
+    val managedActors: Stream[IO, Int] =
+      Stream.bracket(acquire)(release).flatMap(_ => savedJLActors)
+
+    managedActors.compile.drain.as(ExitCode.Success)
+  }
+}
+
+case class DatabaseConnection(connection: String) extends AnyVal
